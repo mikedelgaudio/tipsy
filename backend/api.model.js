efdb76257833439236ba24c90122a7736fcb9dd8
Mike DelGaudio <mdelgaud@stevens.edu>
2021-08-29 17:48:52 -0400

    working backend response but need to break apart into funcs

---
+++ backend/api.model.js
@@ -0,0 +1,15 @@
+const CALCULATE_INPUT = {
+  persons: [
+    {
+      id: "my_id",
+      items: [
+        {
+          price: 12.0,
+        },
+      ],
+    },
+  ],
+  totals: {
+    overall: 12.0,
+  },
+};
--- backend/app.js
+++ backend/app.js
@@ -16,76 +16,93 @@ const isParamsValid = (userInput) => {
   return true;
 };
 
-const DEMO_INPUT_DATA = {
-  persons: [
-    {
-      id: 1245,
-      items: [
-        {
-          price: 12.0,
-        },
-      ],
-    },
-  ],
-  total: 12.0,
-};
-
-const DEMO_OUTPUT_DATA = {
-  persons: [
-    {
-      id: 1245,
-      items: [
-        {
-          price: 12.0,
-        },
-      ],
-      tipDollars: 0,
-      tipPercentage: 0,
-      subtotalBeforeTip: 12.0,
-      subtotal: 12.0,
-    },
-  ],
-
-  totals: {
-    tipAndTax: 12.0,
-    tipPercentage: 0,
-    overall: 12.0,
-  },
-};
-
-class Calculation {
-  constructor(options) {
-    this.totals = {
-      tipAndTax: options.totals.tipAndTax || 0,
-      tipPercentage: options.totals.tipPercentage || 0,
-      overall: options.totals.overall || 0,
-    };
-  }
-}
-
 app.post("/calculate", (req, res) => {
   const userInput = req.body;
   const persons = userInput?.persons;
+  const personsLen = persons.length;
+  const totals = userInput?.totals;
+
   // if (userInput?.)
 
-  // persons array
-  // for each person loop each food item
+  const output = {
+    persons: [],
+    totals: {
+      subtotals: 0,
+      tipAndTax: 0,
+      tipPercentage: 0,
+      overall: 0,
+    },
+  };
 
-  const output = new Calculation();
+  try {
+    output.totals.overall = parseFloat(totals?.overall);
+  } catch (err) {
+    res.status(500).send("Unable to calculate overall");
+    return;
+  }
 
-  for (let personIndex = 0; personIndex < persons.length; personIndex++) {
-    const items = person?.items;
+  for (let i = 0; i < personsLen; i++) {
+    //foreach person
+    const items = persons[i]?.items;
+    const person = {
+      id: persons[i]?.id,
+      tipDollars: 0,
+      tipPercentage: 0,
+      subtotalBeforeTip: 0,
+      subtotal: 0,
+    };
+    let subtotalBeforeTip = 0;
 
-    for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+    for (let j = 0; j < items.length; j++) {
+      // for each item
       try {
-        subtotalBeforeTip += parseFloat(items[itemIndex]?.price);
+        subtotalBeforeTip += parseFloat(items[j]?.price);
       } catch (e) {
         res.status(500).send("Unable to calculate subtotals");
         return;
       }
     }
+
+    person.subtotalBeforeTip = subtotalBeforeTip;
+    output.totals.subtotals += subtotalBeforeTip;
+
+    output.persons.push(person);
   }
+
+  // Make sure to add validation if user types in wrong total overall
+
+  output.totals.tipAndTax = output.totals.overall - output.totals.subtotals;
+
+  output.totals.tipPercentage = (
+    output.totals?.overall / output.totals?.subtotals
+  ).toFixed(2);
+
+  for (let i = 0; i < personsLen; i++) {
+    // for each person
+    output.persons[i].tipDollars =
+      (output.persons[i]?.subtotalBeforeTip / output.totals?.subtotals) *
+      output.totals.tipAndTax;
+
+    output.persons[i].subtotal =
+      output.persons[i]?.subtotalBeforeTip + output.persons[i]?.tipDollars;
+
+    if (output.totals?.tipAndTax > 0) {
+      const decimalTip =
+        output.persons[i]?.tipDollars / output.totals?.tipAndTax;
+      output.persons[i].tipPercentage = (decimalTip * 100).toFixed(2);
+    } else {
+      output.persons[i].tipPercentage = "0.00";
+    }
+  }
+
+  res.json(output);
 });
+
+const determinePercentage = (input) => {
+  if (!input || typeof input !== "number" || input < 0) {
+    return "0.00";
+  }
+};
 
 app.listen(PORT, () => {
   console.log(`Server live at port ${PORT}`);
