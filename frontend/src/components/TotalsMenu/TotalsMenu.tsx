f71c0ee747deced141b04054ea32a696757b5255
Mike DelGaudio <mdelgaud@stevens.edu>
2021-09-08 19:49:30 -0400

    responsive layout for totals component

--- frontend/src/components/TotalsMenu/TotalsMenu.tsx
+++ frontend/src/components/TotalsMenu/TotalsMenu.tsx
@@ -4,23 +4,37 @@ function TotalsMenu() {
   return (
     <div className="totalsMenuView">
       <div className="totalRow">
-        <h1>Totals</h1>
-        <h1>$131.21</h1>
+        <h1 className="totalText">Totals</h1>
+        <h1 className="totalText">$1031.21</h1>
       </div>
 
       <div className="totalsBreakdownWrapper">
         <ul className="totalsBreakdownList">
           <li className="totalBreakdownItem">
-            <span>Before Tip and Tax</span>
+            <span className="totalBreakdownDesc">Before Tip and Tax</span>
           </li>
           <li className="totalBreakdownItem">
-            <span>Tip and Tax %</span>
+            <span className="totalBreakdownDesc">Tip and Tax %</span>
           </li>
           <li className="totalBreakdownItem">
-            <span>Tip and Tax $</span>
+            <span className="totalBreakdownDesc">Tip and Tax $</span>
           </li>
           <li className="totalBreakdownItem">
-            <span>Final</span>
+            <span className="totalBreakdownDesc">Final</span>
+          </li>
+        </ul>
+        <ul className="totalsBreakdownList">
+          <li className="totalBreakdownItem">
+            <span>$100.50</span>
+          </li>
+          <li className="totalBreakdownItem">
+            <span>20%</span>
+          </li>
+          <li className="totalBreakdownItem">
+            <span>$12</span>
+          </li>
+          <li className="totalBreakdownItem">
+            <span>$1086.99</span>
           </li>
         </ul>
       </div>
