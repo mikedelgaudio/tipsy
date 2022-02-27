101d882804767f095bd9cb6c8af64099151b2733
Mike DelGaudio <delgaudiomike@gmail.com>
2022-02-27 17:58:11 -0500

    adding in navbar changes

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import "./Navbar.css";
+import { APP_VERSION } from "../../utilities/variables";
 
 function Navbar() {
   return (
@@ -6,7 +7,12 @@ function Navbar() {
       <div className="nav-container">
         <div className="nav-wrapper">
           <h1>Tipsy</h1>
-          <small>Your best night out calculator</small>
+          <div>
+            <p>{APP_VERSION}</p>
+            <a href="https://github.com/mikedelgaudio/tipsy" target="_blank">
+              @mikedelgaudio
+            </a>
+          </div>
         </div>
       </div>
     </nav>
