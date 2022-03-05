0a6edf9cc263bbaa1a2e9bf3693fef823654afad
Mike DelGaudio <delgaudiomike@gmail.com>
2022-03-05 16:59:00 -0500

    revert toasts'

--- frontend/src/components/ResultsView/ResultsView.tsx
+++ frontend/src/components/ResultsView/ResultsView.tsx
@@ -1,10 +1,13 @@
 import ItemMenu from "./ItemMenu/ItemMenu";
 import "./ResultsView.css";
 import TotalsMenu from "./TotalsMenu/TotalsMenu";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function ResultsView() {
   return (
     <form className="results-view" onSubmit={e => e.preventDefault()}>
+      <ToastContainer role="alert" />
       <ItemMenu />
       <TotalsMenu />
     </form>
---
+++ frontend/src/components/shared/toasts/toasts.tsx
@@ -0,0 +1,30 @@
+import { toast } from "react-toastify";
+
+// Use this for the error boundary
+export const errorToast = (msg = "") => {
+  toast.error(msg || "Unexpected error occurred.", {
+    position: "top-right",
+    autoClose: false,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const warnToast = (msg = "") => {
+  toast.warn(msg || "Unexpected warning occurred.", {
+    position: "top-right",
+    autoClose: false,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const dismissToast = () => {
+  toast.dismiss();
+};
