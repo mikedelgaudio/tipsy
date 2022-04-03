fe4b018010b03a22e0976d3135c8cd7247d83d8a
Mike DelGaudio <delgaudiomike@gmail.com>
2022-04-03 16:50:41 -0400

    update styling in subheader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "./components/Navbar";
-import { ResultsView } from "./components/Results-View";
+// import { ResultsView } from "./components/Results-View";
 import { WelcomeView } from "./components/WelcomeView";
 import { saveState } from "./redux/localStorage";
 import store from "./redux/store";
--- frontend/src/components/WelcomeView/welcome-view.component.tsx
+++ frontend/src/components/WelcomeView/welcome-view.component.tsx
@@ -1,17 +1,19 @@
 import { ExternalLinkIcon } from "../shared/icons";
-import "./welcomeView.component.css";
+import "./welcome-view.component.css";
 
 function WelcomeView() {
   return (
     <section className="welcome-container container">
       <div className="welcome-col">
         <h1 className="welcome-header">The smarter way to get paid back</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error natus,
-          quasi perspiciatis aspernatur nihil voluptatum. Quaerat officia est
-          voluptatem placeat minus, excepturi quia ut exercitationem itaque
-          molestias facere repellendus? Ratione.
-        </p>
+        <div className="welcome-subheader-container">
+          <p>Paid for a night out with friends or a large dinner party?</p>
+          <p>
+            Add the items from your receipt and determine how much each person
+            owes you with tip and tax included.
+          </p>
+        </div>
+
         <div className="welcome-action-row">
           <button className="btn secondary">
             GitHub{" "}
