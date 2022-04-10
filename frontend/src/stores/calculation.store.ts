a487d0f3cbc47556c4a14d50f0950a1781504655
Mike DelGaudio <delgaudiomike@gmail.com>
2022-04-10 15:25:32 -0400

    resolve saving state locally

--- frontend/src/stores/calculation.store.ts
+++ frontend/src/stores/calculation.store.ts
@@ -1,13 +1,19 @@
 import { makeAutoObservable } from "mobx";
+import { makePersistable } from "mobx-persist-store";
 import { CalculationStateMobx } from "../models/calculation-state.mobx.model";
 import { LOCAL_STORAGE_VAR } from "../utilities/variables";
 import { DEFAULT_STATE } from "./defaults/initial.calculation.default";
 
 export class CalculationStore {
-  private state: CalculationStateMobx = DEFAULT_STATE;
+  state: CalculationStateMobx = DEFAULT_STATE;
 
   constructor() {
     makeAutoObservable(this);
+    makePersistable(this, {
+      name: LOCAL_STORAGE_VAR,
+      properties: ["state"],
+      storage: window.localStorage,
+    });
   }
 
   reset() {
