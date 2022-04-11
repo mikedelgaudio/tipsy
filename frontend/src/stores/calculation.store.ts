97522e1c7c9e81b756670b64eac37f1154b8381f
Mike DelGaudio <delgaudiomike@gmail.com>
2022-04-10 21:15:17 -0400

    create refactored toast service

--- frontend/src/stores/calculation.store.ts
+++ frontend/src/stores/calculation.store.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable } from "mobx";
 import { makePersistable } from "mobx-persist-store";
 import { v4 as uuidv4 } from "uuid";
 import { CalculationStateMobx } from "../models/calculation-state.mobx.model";
+import { ToastService } from "../services/toast.service";
 import { sanitizeCurrency } from "../utilities/sanitize";
 import { LOCAL_STORAGE_VAR } from "../utilities/variables";
 import { DEFAULT_STATE } from "./defaults/initial.calculation.default";
@@ -9,7 +10,7 @@ import { DEFAULT_STATE } from "./defaults/initial.calculation.default";
 export class CalculationStore {
   state: CalculationStateMobx = DEFAULT_STATE;
 
-  constructor() {
+  constructor(private readonly toastService: ToastService) {
     makeAutoObservable(this);
     makePersistable(this, {
       name: LOCAL_STORAGE_VAR,
@@ -21,12 +22,18 @@ export class CalculationStore {
   editEventTotal(total: string) {
     const { error, parsedFloat, parsedString } = sanitizeCurrency(total);
 
+    const TOAST_ID = "EVENT_TOTAL";
+
     if (error) {
-      // errorService.error()
+      this.toastService.error(
+        TOAST_ID,
+        "Error formatting event total price. Please verify your input.",
+      );
       this.state.event.errors.total = true;
       return;
     }
 
+    this.toastService.clear(TOAST_ID);
     this.state.event.errors.total = false;
     this.state.event.total = parsedString;
     this.state.event.totalFloat = parsedFloat;
