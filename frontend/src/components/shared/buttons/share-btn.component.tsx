03f5f036cc310592fd66a438e5277e64cafb4c92
Mike DelGaudio <delgaudiomike@gmail.com>
2022-05-13 19:19:27 -0400

    begin purge of svg icons components for size

--- frontend/src/components/shared/buttons/share-btn.component.tsx
+++ frontend/src/components/shared/buttons/share-btn.component.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 import { useContext } from "react";
 import { ToastService } from "../../../services/toast.service";
 import { StoreContext } from "../../../store.context";
-import { CopyIcon, MailIcon, ShareIcon } from "../icons";
+import { CopyIcon, ShareIcon } from "../icons";
 import { Modal } from "../modal";
 
 const ShareBtn = observer(
@@ -60,7 +60,17 @@ const ShareBtn = observer(
             onClick={() => triggerEmail()}
           >
             <span className="btn-wrapper">
-              <MailIcon className="icons" />
+              <svg
+                className="icons"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 64c26.5 0 48 21.49 48 48 0 15.1-7.1 29.3-19.2 38.4L275.2 313.6a32.1 32.1 0 0 1-38.4 0L19.2 150.4C7.113 141.3 0 127.1 0 112c0-26.51 21.49-48 48-48h416zM217.6 339.2a63.9 63.9 0 0 0 76.8 0L512 176v208c0 35.3-28.7 64-64 64H64c-35.35 0-64-28.7-64-64V176l217.6 163.2z"
+                />
+              </svg>
               <span className="btn-text-allow-full">Email</span>
             </span>
           </button>
